4a0648f71032c758642c568d2a301c158f4ae6be
Zagidulin Ranis <zagidulin.ran@yandex.ru>
2025-04-03 23:23:46 +0300

    initial commit

---
+++ src/test/java/ru/spbpu/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbpu.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
